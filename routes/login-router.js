e258c548c03e623772a0df0ddcfbb21074510345
show3567 <ydong011@fiu.edu>
2021-04-15 09:19:32 -0400

    init

---
+++ routes/login-router.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt'); // hush password
+const Joi = require("@hapi/joi");
+
+const { User } = require('../models/user');
+
+router.post("/", async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({ name: req.body.userName });
+    if (!user) return res.status(400).send('Invalid email or password.');
+
+    const validPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+    ); // compare password;
+
+    if (!validPassword) return res.status(400).send('Invalid email or password.');
+
+    const token = user.generateAuthToken();
+    
+    res.send({
+        userName: user.name,
+        bearerToken: token,
+        isAuthenticated: user.isAdmin,
+
+        canAccessProducts: user.claim.canAccessProducts,
+        canAddProducts: user.claim.canAddProducts,
+        canSaveProduct: user.claim.canSaveProduct,
+        canAccessCategories: user.claim.canAccessCategories,
+        canAddCategory: user.claim.canAddCategory,
+    });
+});
+
+const validate = (user) => {
+    const schema = Joi.object({
+        userName: Joi.string().min(3).max(255).required(),
+        password: Joi.string().min(3).max(255).required(),
+    });
+    return schema.validate(user);
+}
+
+module.exports = router;
